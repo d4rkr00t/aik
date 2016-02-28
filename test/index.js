27b518359e096bfe91aa11b119d1395d934b7708
Stanislav Sysoev <d4rkr00t@gmail.com>
2016-02-28 16:42:16 +0300

    test(webpack): Test with react flag enabled

--- test/index.js
+++ test/index.js
@@ -4,5 +4,5 @@ import 'babel-core/register';
 import aik from '../lib/';
 
 test('aik', () => {
-  return aik('cli.js', { host: 'localhost', port: 1234, ngrok: true, cssmodules: true });
+  return aik('cli.js', { host: 'localhost', port: 1234, ngrok: true, cssmodules: true, react: true });
 });
