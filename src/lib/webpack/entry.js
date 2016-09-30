2d2502a88c511a932fe1356ac8c8a47a9065e67b
Stanislav Sysoev <d4rkr00t@gmail.com>
2016-09-30 23:17:52 +0300

    feat(webpack): Entry name depends on file name

--- src/lib/webpack/entry.js
+++ src/lib/webpack/entry.js
@@ -1,13 +1,26 @@
 /* @flow */
 
+import path from 'path';
 import { resolveToCwd } from './helpers';
+
+/**
+ * Build entry name from given filename.
+ *
+ * index.js -> index
+ * src/index.js -> index
+ * index.sth.js -> index
+ */
+export function buildEntryName(filename:string) : string {
+  return path.basename(filename).split('.')[0];
+}
 
 /**
  * Entry for production build.
  */
 export function entryProd(filename:string) : Entry {
+  const entry = buildEntryName(filename);
   return {
-    app: [resolveToCwd(filename)]
+    [entry]: [resolveToCwd(filename)]
   };
 }
 
@@ -16,10 +29,11 @@ export function entryProd(filename:string) : Entry {
  * Entry for dev server.
  */
 export function entryDev(filename:string, flags:CLIFlags) : Entry {
+  const entry = buildEntryName(filename);
   const host = flags.host === '::' ? 'localhost' : flags.host;
 
   return {
-    app: [
+    [entry]: [
       `${require.resolve('webpack-dev-server/client')}?http://${host}:${flags.port}/`,
       require.resolve('webpack/hot/dev-server'),
       resolveToCwd(filename)
