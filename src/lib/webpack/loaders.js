dac24c1be1187b5cccaffd3acec90be1d4ca1946
Stanislav Sysoev <d4rkr00t@gmail.com>
2016-10-01 09:22:50 +0300

    feat(webpack): Add more extensions to url and file loaders

--- src/lib/webpack/loaders.js
+++ src/lib/webpack/loaders.js
@@ -91,12 +91,12 @@ export function loaders(flags:CLIFlags, params:AikParams) : Loader[] {
       loader: require.resolve('json-loader')
     },
     {
-      test: /\.(mp4|webm)$/,
+      test: /\.(mp4|webm|wav|mp3|m4a|aac|oga)$/,
       loader: require.resolve('url-loader'),
       query: { limit: 1000 }
     },
     {
-      test: /\.(jpg|png|gif|eot|svg|ttf|woff|woff2)$/,
+      test: /\.(jpg|jpeg|png|gif|eot|svg|ttf|woff|woff2)$/,
       loader: require.resolve('file-loader'),
       query: isProd && {
         name: '[name].[hash:8].[ext]'
