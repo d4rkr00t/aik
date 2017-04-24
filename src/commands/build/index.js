7015901ee1ae4304d8ff4505fb7916187b5dfc56
Stanislav Sysoev <d4rkr00t@gmail.com>
2017-04-24 19:18:44 +1000

    feat: Pre-installing npm modules if project contains package.json

--- src/commands/build/index.js
+++ src/commands/build/index.js
@@ -1,6 +1,7 @@
 /* @flow */
 import runWebpackBuilder from "./webpack-build";
 import createParams from "./../../utils/params";
+import preinstallNpmModules from "../../preinstall-npm-modules";
 
 /**
  * Aik build command
@@ -8,5 +9,6 @@ import createParams from "./../../utils/params";
 export default function aikBuild(input: string[], flags: CLIFlags): Promise<*> {
   const [filename] = input;
   const params = createParams(filename, flags, "", true);
+  preinstallNpmModules(process.cwd());
   return runWebpackBuilder(filename, flags, params);
 }
