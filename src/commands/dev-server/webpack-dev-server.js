099e13bdfb8b8e28ee84fef7b8b6567640b41944
Stanislav Sysoev <d4rkr00t@gmail.com>
2017-04-24 19:41:02 +1000

    refactor: Use async/await for detecting free port

--- src/commands/dev-server/webpack-dev-server.js
+++ src/commands/dev-server/webpack-dev-server.js
@@ -110,79 +110,80 @@ export function createWebpackCompiler(
 /**
  * Creates webpack dev server.
  */
-export default function createWebpackDevServer(
+export default async function createWebpackDevServer(
   filename: string,
   flags: CLIFlags,
   params: AikParams
 ): Promise<Object> {
-  return detectPort(parseInt(flags.port, 10), flags.host).then(port => {
-    if (port !== flags.port) {
-      flags.oldPort = flags.port;
-      flags.port = port;
+  const port = await detectPort(parseInt(flags.port, 10), flags.host);
+
+  if (port !== flags.port) {
+    flags.oldPort = flags.port;
+    flags.port = port;
+  }
+
+  let server: WebpackDevServer; // eslint-disable-line
+  const invalidate = (errors: string[]) => {
+    if (!server) return;
+
+    const error = errors[0] || "";
+    const fileWithError = (error.match(/Error in (.+)\n/) || [])[1];
+    let moduleName = (error.match(
+      /Module not found: Error: Cannot resolve module '(.+)'/
+    ) || [])[1];
+
+    if (!moduleName) return;
+
+    moduleName = moduleName.replace(/'/gim, "");
+
+    try {
+      resolveModule.sync(moduleName, { basedir: process.cwd() });
+      devServerRestartMsg(moduleName);
+      server.close();
+      createWebpackDevServer(filename, flags, params);
+    } catch (e) {
+      // eslint-disable-line
+      devServerModuleDoesntExists(moduleName, fileWithError);
     }
+  };
 
-    let server: WebpackDevServer; // eslint-disable-line
-    const invalidate = (errors: string[]) => {
-      if (!server) return;
+  const config = webpackConfigBuilder(filename, flags, params);
+  const compiler = createWebpackCompiler(
+    filename,
+    flags,
+    params,
+    config,
+    invalidate
+  );
 
-      const error = errors[0] || "";
-      const fileWithError = (error.match(/Error in (.+)\n/) || [])[1];
-      let moduleName = (error.match(
-        /Module not found: Error: Cannot resolve module '(.+)'/
-      ) || [])[1];
+  server = new WebpackDevServer(compiler, {
+    // Enable gzip compression of generated files.
+    compress: true,
 
-      if (!moduleName) return;
+    // Silence WebpackDevServer's own logs since they're generally not useful.
+    // It will still show compile warnings and errors with this setting.
+    clientLogLevel: "none",
+    historyApiFallback: true,
+    hot: true,
+    overlay: {
+      errors: true
+    },
+    quiet: true,
+    stats: { colors: true }
+  });
 
-      moduleName = moduleName.replace(/'/gim, "");
+  return new Promise((resolve, reject) => {
+    server.listen(flags.port, flags.host, err => {
+      if (err) return reject(err);
 
-      try {
-        resolveModule.sync(moduleName, { basedir: process.cwd() });
-        devServerRestartMsg(moduleName);
-        server.close();
-        createWebpackDevServer(filename, flags, params);
-      } catch (e) {
-        // eslint-disable-line
-        devServerModuleDoesntExists(moduleName, fileWithError);
-      }
-    };
-    const config = webpackConfigBuilder(filename, flags, params);
-    const compiler = createWebpackCompiler(
-      filename,
-      flags,
-      params,
-      config,
-      invalidate
-    );
+      server.use(
+        historyApiFallback({
+          disableDotRule: true,
+          htmlAcceptHeaders: ["text/html"]
+        })
+      );
 
-    server = new WebpackDevServer(compiler, {
-      // Enable gzip compression of generated files.
-      compress: true,
-
-      // Silence WebpackDevServer's own logs since they're generally not useful.
-      // It will still show compile warnings and errors with this setting.
-      clientLogLevel: "none",
-      historyApiFallback: true,
-      hot: true,
-      overlay: {
-        errors: true
-      },
-      quiet: true,
-      stats: { colors: true }
-    });
-
-    return new Promise((resolve, reject) => {
-      server.listen(flags.port, flags.host, err => {
-        if (err) return reject(err);
-
-        server.use(
-          historyApiFallback({
-            disableDotRule: true,
-            htmlAcceptHeaders: ["text/html"]
-          })
-        );
-
-        resolve(server);
-      });
+      resolve(server);
     });
   });
 }
